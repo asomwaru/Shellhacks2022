f919a6f9e9f7a764af975e68462964a053b051fb
asomwaru <asomwaru01@gmail.com>
2022-09-10 10:08:19 -0400

    Created new storage bucket

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -11,3 +11,17 @@ resource "google_storage_bucket" "ocr_file_processing" {
     max_age_seconds = 3600
   }
 }
+
+resource "google_storage_bucket" "audio_files" {
+  name          = "audio-processing-files"
+  location      = "US"
+  force_destroy = false
+
+  # uniform_bucket_level_access = true
+  cors {
+    origin          = ["*"] # TODO: Change origin when frontend is deployed
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
