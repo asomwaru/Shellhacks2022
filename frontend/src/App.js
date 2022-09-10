0a6db563104ac4e78dc108187b8796b0b26150d0
jl01128 <jackie01128@gmail.com>
2022-09-10 10:04:13 -0400

    buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,17 @@ function App() {
       <header className="">
         <h1 className='text-white font-bold text-3xl'>Gibbrish</h1>
       </header>
+      <body>
+
+        <button class="bg-cyan-500 hover:bg-cyan-6000">Translate</button>
+
+        or
+
+        <button class="bg-red-500 ">Simplify</button>
+
+      </body>
+      
+      
     </div>
   );
 }
